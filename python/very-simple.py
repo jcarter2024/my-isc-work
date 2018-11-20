b7a833fb5d3ed61763151464562b968b6bc75006
user01 <user01@see-l11232.leeds.ac.uk>
2018-11-20 15:06:03 +0000

    added a python folder of exercises

---
+++ python/very-simple.py
@@ -0,0 +1,36 @@
+#Remember, hashes exclude comments
+
+print(1+2)
+print('Charles ' + 'Darwin')
+
+#We can assign varibales with strings to print
+
+Planet = 'Pluto'
+print(Planet)
+
+#Python can reassign variable types
+print(int('2') + 3) #'2' is a string
+#returns 5 
+
+#integers have unlimited precisin but floats have 64 bit allocation
+#complex numbers have two 64 bit (one for each component)
+
+#multiplying a string by an integer gives a concatonation
+print(Planet * 2)
+
+#Rounded division with //
+
+#Python uses in place forms of binary operators
+years=500
+years  +=1 #equivalent to years = years +1 (shorthand)
+
+#Comparisons:
+#3 < 5 less than 
+# 3!= 5 Logic not equal to
+
+
+
+
+exit
+
+
