744166c46f5997a4f06e099e4b28a18c45c49ec2
jcarter2024 <jcarter2024@gmail.com>
2019-09-03 14:08:16 +0100

    exercises 2 annotated

--- exercise_2/generateandmodify.py
+++ exercise_2/generateandmodify.py
@@ -1,9 +1,14 @@
 import numpy as np
 
-arr= np.array((range(4), range(10, 14)))
-print(arr.reshape(2, 2, 2))
-print(np.transpose(arr))
+#this programme is for making some modifications to a generated array 
 
-print(arr.flatten())
-print(np.array(arr, dtype = 'float32'))
+arr= np.array((range(4), range(10, 14))) #first we make an array of 2 rows, each row has elements of the given range 
+                                         #and demonstrate that this can be done with the range function
+#print(arr)
+
+print(arr.reshape(4, 1, 2)) #we can reshape the array into 3 dimensiions (rows, depth, columns)
+print(np.transpose(arr)) #simple transpose operation
+
+print(arr.flatten()) #make simple row vector
+print(np.array(arr, dtype = 'float32')) #make float 
 
--- exercise_2/interogate.py
+++ exercise_2/interogate.py
@@ -1,7 +1,8 @@
 import numpy as np
 
-arr= np.array((range(4), range(10, 14)))
-print(arr.shape)
-print(arr.size)
-print(arr.max())
-print(arr.min())
+#lets make an array of 2 rows with elements in the given ranges
+arr= np.array((range(4), range(-14, -10)))
+print(arr.shape) #the array shape should be a 2x4
+print(arr.size) #the size of the array should be 8 elements
+print(arr.max()) #whats the maximum element value, its 13
+print(arr.min()) #whats the minimum element value, its 0, for negatives it will show the lowest, i.e. no modulus
